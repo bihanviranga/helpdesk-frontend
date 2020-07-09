4d51e96c8caba40ee991c87a48f5e7094f6fc57b
dhfernando <hdilshan.fernando11@gmail.com>
2020-07-10 00:25:13 +0530

    dev fontend desing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {useSelector , useDispatch} from 'react-redux'
 import {Provider} from 'react-redux'
 import store from './redux/store'
 import Header from './components/Header'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import Home from './pages/Home'
 import KnowledgeBase_index from './pages/KnowledgeBase/knowledgeBase_index'
@@ -15,6 +17,8 @@ import UserLogin from './pages/User/UserLogin'
 import MyProfile from './pages/User/MyProfile'
 import CreateTicket from './pages/Ticket/CreateTicket'
 import Company from './pages/Company/CompanyIndex'
+import TestComponent from './components/TestComponent'
+import Container from '@material-ui/core/Container';
 
 import {
   BrowserRouter as Router,
@@ -22,26 +26,60 @@ import {
   Route
 } from 'react-router-dom'
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
 
 function App() {
-
+  const classes = useStyles();
   return (
     <>
       <Provider store={store}>
         <div className="App"> 
             <Router>
               <Header />
-              <Switch> 
-                  <Route exact path="/" component={Home} />
-                  <Route path="/KnowledgeBase_index" component={KnowledgeBase_index} />
-                  <Route path="/CreateArticle" component={CreateArticle} />
-                  <Route path="/User" component={UserIndex} />
-                  <Route path="/UserRegistration" component={UserRegistration} />
-                  <Route path="/UserLogin" component={UserLogin} />
-                  <Route path="/CreateTicket" component={CreateTicket} />
-                  <Route path="/MyProfile" component={MyProfile} />
-                  <Route path="/Company" component={Company} />
-               </Switch>
+            
+                  <Container  maxWidth={"xl"}>
+                    <Switch> 
+                        <Route exact path="/" component={Home} />
+                        <Route path="/KnowledgeBase_index" component={KnowledgeBase_index} />
+                        <Route path="/CreateArticle" component={CreateArticle} />
+                        <Route path="/User" component={UserIndex} />
+                        <Route path="/UserRegistration" component={UserRegistration} />
+                        <Route path="/UserLogin" component={UserLogin} />
+                        <Route path="/CreateTicket" component={CreateTicket} />
+                        <Route path="/MyProfile" component={MyProfile} />
+                        <Route path="/Company" component={Company} />
+                        <Route path="/TestComponent" component={TestComponent} />
+                    </Switch>
+                  </Container>
             </Router>
         </div>
       </Provider>
--- src/pages/User/UserLogin.jsx
+++ src/pages/User/UserLogin.jsx
@@ -2,8 +2,61 @@ import React , {useState , useEffect }from 'react'
 import {useSelector , useDispatch} from 'react-redux'
 import { loginUser } from '../../redux'
 
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+//custom style
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+
+  //component
+  function CopyrightComponent() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          99x Help Desk
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
 function UserLogin() {
     const dispatch = useDispatch();
+    const classes = useStyles();
+
     const intLoginDetails = {
         UserNameOrEmail:null,
         password:null
@@ -14,14 +67,38 @@ function UserLogin() {
 
     return (
         <div>
-            <form>
-                <input type="text" name="UserNameOrEmail" placeholder="User Name Or Email" onChange={e=>  setLoginDetails({ ...LoginDetails , UserNameOrEmail : e.target.value })}/>
-                <input type="password" name="password" placeholder="password" onChange={e=>  setLoginDetails({ ...LoginDetails , password : e.target.value })}/>
-                <button onClick={(e)=>{
-                    dispatch(loginUser(LoginDetails))
-                    e.preventDefault();
-                }}>Login</button>
-            </form>
+            <Container component="main" maxWidth="sm">
+                <CssBaseline />
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5"> Login </Typography>
+                        <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                            <TextField variant="outlined" required fullWidth name="UserNameOrEmail" label="UserName Or Email"
+                                onChange={e=>  setLoginDetails({ ...LoginDetails , UserNameOrEmail : e.target.value })}   
+                            />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <TextField variant="outlined" required fullWidth name="Password" label="Password"
+                                type="password" onChange={e=>  setLoginDetails({ ...LoginDetails , password : e.target.value })}
+                            />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit" fullWidth  variant="contained" color="primary"
+                            className={classes.submit} onClick={(e)=>{
+                                dispatch(loginUser(LoginDetails))
+                                e.preventDefault();
+                            }} > Login </Button>
+                        </form>
+                    </div>
+                    <Box mt={5}>
+                        <CopyrightComponent />
+                    </Box>
+                </Container>
         </div>
     )
 }
